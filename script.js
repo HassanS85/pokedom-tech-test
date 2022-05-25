dab51a718aee8c0d7c58ba16f3de49e3d86f9143
Hassan <hassan.seedat07@gmail.com>
2022-05-25 16:35:44 +0100

    updated with javascript

---
+++ script.js
@@ -0,0 +1,34 @@
+import {pokemonArray} from "./data/pokemon";
+console.log(pokemonArray());
+
+//display the relevant cards on here 
+
+
+const h1 = document.querySelectorAll(".h1")
+const cardContainer = document.querySelectorAll(".card-container");
+console.log('hello world')
+
+
+
+cardContainer.innerHTML = 
+'<div class= "Card"></div>'
+    '<div class= "card__image"></div>'
+        '<div class ="card__content".</div>'
+            '<div class ="card__heading"></div>'
+                '<div class="card__text"></div>'
+
+h1.innerText = "Placeholder Text"  
+
+
+// If you haven't used imports in JavaScript before, take a look at the MDN Docs. You will need to find out how to import the pokemonArray from the pokemon.js file into the script.js file
+// Look at the SCSS file for clues to what the HTML cards should look like
+// We're looking for clean code here, make sure you remove extra console.logs and commented code, and remember to format your code!
+
+
+
+
+
+
+
+
+
